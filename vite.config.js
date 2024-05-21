703aeebb8b4f9b85437adcab803c6d3056af5777
Chris Mayfield <mayfiecs@jmu.edu>
2024-05-21 11:05:59 -0400

    run formatter on html and css files

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite'
 
-
-
 const PRAXLY_PATH = process.env.PRAXLY_PATH || "/";
 console.log("\n\n\nPRAXLY_PATH", PRAXLY_PATH, '\n\n\n');
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: PRAXLY_PATH,
-  build: {
-    outDir: "dist",
-  },
+    base: PRAXLY_PATH,
+    build: {
+        outDir: "dist",
+    },
 });
