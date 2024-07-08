2ce019fd9b82b5cf1059837d793abe019bb510f8
Chris Mayfield <mayfiecs@jmu.edu>
2024-07-08 13:08:53 -0600

    add rollupOptions to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,20 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 const PRAXLY_PATH = process.env.PRAXLY_PATH || "/";
 console.log("\n\n\nPRAXLY_PATH", PRAXLY_PATH, '\n\n\n');
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: PRAXLY_PATH,
-    build: {
-        outDir: "dist",
+  base: PRAXLY_PATH,
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        embed: resolve(__dirname, 'embed.html'),
+        mainPage: resolve(__dirname, 'main.html')
+      }
     },
+  },
 });
